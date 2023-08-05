67901a7a00c1e4e39f30438029a46c179089c329
Roman Permyakov <info@presstomsk.ru>
2023-08-06 01:42:02 +0700

    [Phonebook database] Add error handler middleware, correct api

--- index.js
+++ index.js
@@ -23,16 +23,39 @@ app.get('/api/persons', (request, response) => {// rest get all
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {//find person by Id
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  }) 
+app.get('/api/persons/:id', (request, response, next) => {//find person by Id
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }      
+    })
+    .catch(error => next(error)) 
 })
 
-app.delete('/api/persons/:id', (request, response) => { //rest delete by Id
-  Person.findByIdAndDelete(request.params.id).then(person => {
-    response.status(204).end()
-  }) 
+app.delete('/api/persons/:id', (request, response, next) => { //rest delete by Id
+  Person.findByIdAndRemove(request.params.id)
+    .then(person => {
+      response.status(204).end()
+    })
+    .catch(error => next(error)) 
+})
+
+app.put('/api/persons/:id', (request, response, next) => {//rest put
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number || "",
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
   
 app.post('/api/persons', (request, response) => { // post add person
@@ -42,7 +65,7 @@ app.post('/api/persons', (request, response) => { // post add person
     return response.status(400).json({ 
       error: 'name or number missing' 
     })
-  }  
+  } 
   
   const person = new Person({
     name: body.name,
@@ -53,6 +76,18 @@ app.post('/api/persons', (request, response) => { // post add person
     response.json(savedPerson)
   }) 
 })  
+
+const errorHandler = (error, request, response, next) => {//express error handler
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)// error handler
 
 const PORT = process.env.PORT// for deploy port from .env
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })//listen port
