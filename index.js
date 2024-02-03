97c361cc7bcf1f5868d7546fbd5f9bc79bd58756
Roman Permyakov <permyakovr1984@gmail.com>
2024-02-03 22:22:51 +0700

    [Phonebook database] Add Validation

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ app.post('/api/persons', (request, response) => { // post add person
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  }) 
+  })
+  .catch(error => next(error)) 
 })  
 
 const errorHandler = (error, request, response, next) => {//express error handler
@@ -82,7 +83,8 @@ const errorHandler = (error, request, response, next) => {//express error handle
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })}; 
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -14,9 +14,16 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB:', error.message)
     })
 
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+const personSchema = new mongoose.Schema({   
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8
+    },
 })
     
 personSchema.set("toJSON", {//corect shema
