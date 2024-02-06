82ab7387c9472c41e974794850b4921b41686868
Roman Permyakov <permyakovr1984@gmail.com>
2024-02-06 21:15:25 +0700

    [Blog list] Create project with correct structure

---
+++ part4/Blog list/app.js
@@ -0,0 +1,28 @@
+const config = require('./Utils/config.js')
+const logger = require('./Utils/logger.js')
+const middleware = require('./Utils/middleware.js')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./Controllers/blogs.js')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(result => {logger.info('connected to MongoDB')})
+  .catch((error) => {logger.error('error connecting to MongoDB:', error.message)})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/Blog list/index.js
+++ part4/Blog list/index.js
@@ -1,53 +1,8 @@
-const config = require('./Utils/config.js')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-console.log('connecting to', config.MONGODB_URI)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(result => {console.log('connected to MongoDB')})
-  .catch((error) => {console.log('error connecting to MongoDB:', error.message)})
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+const app = require('./app')
+const config = require('./Utils/config')
+const logger = require('./Utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
+
