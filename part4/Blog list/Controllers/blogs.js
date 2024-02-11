1416d5d4aea05a47136de8da272184ec63f1d574
Roman Permyakov <permyakovr1984@gmail.com>
2024-02-11 23:55:36 +0700

    [Blog List Expansions] Add update and delete api and tests

--- part4/Blog list/Controllers/blogs.js
+++ part4/Blog list/Controllers/blogs.js
@@ -22,4 +22,23 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
